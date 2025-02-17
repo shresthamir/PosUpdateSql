27c23d0d7b828294cd8a75b522df43485f2ee591
shresthamir <shresthamir@outlook.com>
2025-02-17 18:17:12 +0545

    #135339 : Fixed KOT Estimate for PAN Client

--- GetKOTEstimate.sql
+++ GetKOTEstimate.sql
@@ -4,11 +4,11 @@ RETURNS DECIMAL(18,2) AS
 BEGIN
 --DECLARE @TABLENO VARCHAR(50) = 'BAR 01'
 DECLARE @TOTAL DECIMAL(18,2)
-Declare @itmwisesericecharge tinyint, @ServiceChargeRate DECIMAL(5,2)
-select @itmwisesericecharge = ItemWiseSchargeApply, @ServiceChargeRate = ServiceChargeRate from SETTING
+Declare @itmwisesericecharge tinyint, @ServiceChargeRate DECIMAL(5,2), @EnablePanBill tinyint
+select @itmwisesericecharge = ItemWiseSchargeApply, @ServiceChargeRate = ServiceChargeRate, @EnablePanBill = COALESCE(EnablePanBill,0) from SETTING
 SELECT @TOTAL = SUM(GROSS + STAX + VATAMNT) FROM
 (
-	SELECT *, CASE WHEN VAT = 1 THEN (GROSS + STAX)* 0.13 ELSE 0 END VATAMNT FROM
+	SELECT *, CASE WHEN VAT = 1 AND @EnablePanBill = 0 THEN (GROSS + STAX)* 0.13 ELSE 0 END VATAMNT FROM
 	(
 		SELECT M.VAT, M.MCODE, M.MENUCODE, M.DESCA, P.Quantity, M.RATE_A* P.Quantity GROSS,
 		IIF(@itmwisesericecharge = 0 OR M.HASSERVICECHARGE = 1, @ServiceChargeRate, 0) * M.RATE_A * P.Quantity STAX
